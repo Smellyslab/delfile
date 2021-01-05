78ba685fe10a9047d4d74be8e49f10e9a9a97b94
Smellyslab <76969866+Smellyslab@users.noreply.github.com>
2021-01-05 22:10:09 +0000

    Update delfile.c

--- delfile.c
+++ delfile.c
@@ -3,17 +3,21 @@
 int main(int c, char *argv[])
 {
         char status = remove(argv[1]);
-        if( argv < 1 ){
-                printf("\033[01;31margs needed usage: ./delfile <filename>033[0$
+
+        if( argv != '1' ){
+                printf("\033[01;31margs needed usage: delfile <filename>\033[00m\n");
+                printf("\033[01;31mif you want to delete this type: ")
         }
         if( status == 0 ){
-                printf("\033[01;32mfile %s deleted correctly!\033[00m\n",argv[1$
+                printf("\033[01;32mfile %s deleted correctly!\033[00m\n",argv[1]);
         }
         else
         {
-                printf("\033[01;31mfile could not be deleted! try running this $
+                printf("\033[01;31mCould not delete file.\033[00m\n");
         }
 
    return 0;
 }
+
+
 
